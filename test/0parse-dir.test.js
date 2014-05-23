d4153507a24675f605b6124070054fd8ef835f4e
Troy Whiteley <troy@somany.us>
2014-05-22 18:48:56 -0700

    Added test for multiple files

--- test/0parse-dir.test.js
+++ test/0parse-dir.test.js
@@ -5,4 +5,11 @@ describe('parse-dir', function() {
   it('should be a function', function() {
     assert(typeof parseDir === 'function');
   });
+
+  it('should handle multiple files', function(done) {
+    parseDir('test/fixture/**/*', function(err, data) {
+      assert(data.length > 1);
+      done();
+    });
+  });
 });
