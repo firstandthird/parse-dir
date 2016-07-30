2916f2aec736a73f39fcefb1da6134bb913fc46a
Chris <orthagonal1@gmail.com>
2016-07-30 18:31:28 -0500

    coffee parser in coffee test

---
+++ test/coffee.parser.test.js
@@ -0,0 +1,69 @@
+const parseDir = require('../lib/parse-dir');
+const assert = require('assert');
+const fs = require('fs');
+
+const fixture = 'test/fixture/coffeescript/';
+
+describe('coffeescript', () => {
+  it('should be parsed', (done) => {
+    parseDir(`${fixture}users.coffee`, (err, data) => {
+      assert(data[0].parsed);
+      done();
+    });
+  });
+
+  it('should contain filepath', (done) => {
+    parseDir(`${fixture}users.coffee`, (err, data) => {
+      assert(data[0].filepath.indexOf('users.coffee') !== -1);
+      assert(data[0].filepath.indexOf('test') !== -1);
+      assert(data[0].filepath.indexOf('fixture') !== -1);
+      assert(data[0].filepath.indexOf('coffeescript') !== -1);
+      done();
+    });
+  });
+
+  it('should contain filename', (done) => {
+    parseDir(`${fixture}users.coffee`, (err, data) => {
+      assert(data[0].filename === 'users.coffee');
+      done();
+    });
+  });
+
+  it('should contain basename', (done) => {
+    parseDir(`${fixture}users.coffee`, (err, data) => {
+      assert(data[0].basename === 'users');
+      done();
+    });
+  });
+
+  it('should contain extension', (done) => {
+    parseDir(`${fixture}users.coffee`, (err, data) => {
+      assert(data[0].extension === '.coffee');
+      done();
+    });
+  });
+
+  it('should return raw output', (done) => {
+    parseDir(`${fixture}users.coffee`, (err, data) => {
+      assert(data[0].raw === fs.readFileSync(`${fixture}users.coffee`).toString());
+      done();
+    });
+  });
+
+  it('should return parsed output', (done) => {
+    parseDir(`${fixture}users.coffee`, (err, data) => {
+      console.log('----------')
+      console.log(data[0])
+      assert(data[0].contents[0].name === 'John');
+      done();
+    });
+  });
+
+  // Need to figure out a way to test this. Broken JS will just break everything and require just loads an empty object.
+  it.skip('should handle malformed coffee', (done) => {
+    parseDir(`${fixture}malformed.coffee`, (err, data) => {
+      assert(!data[0].parsed);
+      done();
+    });
+  });
+});
