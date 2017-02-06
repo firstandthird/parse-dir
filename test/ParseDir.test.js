6422b3fba6fd10cbad494991804c9443c227caf3
Chris <orthagonal1@gmail.com>
2017-02-06 15:36:38 -0600

    finish ParseDir tests

--- test/ParseDir.test.js
+++ test/ParseDir.test.js
@@ -8,14 +8,17 @@ tape('can construct ParseDir', (assert) => {
 });
 
 tape('ParseDir.sync', (assert) => {
-  const pd = new ParseDir({});
+  const pd = new ParseDir({ async: false });
   const result = pd.sync('test/fixture/json/users.json');
-  console.log('++++++++++++++++++++++++++++++')
-  console.log('++++++++++++++++++++++++++++++')
-  console.log('++++++++++++++++++++++++++++++')
-  console.log('++++++++++++++++++++++++++++++')
-  console.log('++++++++++++++++++++++++++++++')
-  console.log(result)
+  assert.ok(result[0].relativePath);
+  assert.ok(result[0].filepath);
+  assert.ok(result[0].filename);
+  assert.ok(result[0].basename);
+  assert.ok(result[0].extension);
+  assert.ok(result[0].parsed);
+  assert.ok(result[0].raw);
+  assert.equal(result[0].isDirectory, false);
+  assert.equal(result[0].contents[0].name, 'John');
   assert.end();
 });
 
@@ -23,7 +26,15 @@ tape('ParseDir.handleAsync', (assert) => {
   const pd = new ParseDir({});
   pd.handleAsync('test/fixture/json/users.json', (err, result) => {
     assert.equal(err, null);
-    assert.equal(result[0].parsed, true);
+    assert.ok(result[0].relativePath);
+    assert.ok(result[0].filepath);
+    assert.ok(result[0].filename);
+    assert.ok(result[0].basename);
+    assert.ok(result[0].extension);
+    assert.ok(result[0].parsed);
+    assert.ok(result[0].raw);
+    assert.equal(result[0].isDirectory, false);
+    assert.equal(result[0].contents[0].name, 'John');
     assert.end();
   });
 });
