05af117439ba8e12ae15fcbbbd33fa97161dd5de
Chris <orthagonal1@gmail.com>
2017-02-06 15:39:01 -0600

    rename test for index, add labels for parsedir tests

--- test/ParseDir.test.js
+++ test/ParseDir.test.js
@@ -10,13 +10,13 @@ tape('can construct ParseDir', (assert) => {
 tape('ParseDir.sync', (assert) => {
   const pd = new ParseDir({ async: false });
   const result = pd.sync('test/fixture/json/users.json');
-  assert.ok(result[0].relativePath);
-  assert.ok(result[0].filepath);
-  assert.ok(result[0].filename);
-  assert.ok(result[0].basename);
-  assert.ok(result[0].extension);
-  assert.ok(result[0].parsed);
-  assert.ok(result[0].raw);
+  assert.ok(result[0].relativePath, 'should extract required fields');
+  assert.ok(result[0].filepath, 'should extract required fields');
+  assert.ok(result[0].filename, 'should extract required fields');
+  assert.ok(result[0].basename, 'should extract required fields');
+  assert.ok(result[0].extension, 'should extract required fields');
+  assert.ok(result[0].parsed, 'should extract required fields');
+  assert.ok(result[0].raw, 'should extract required fields');
   assert.equal(result[0].isDirectory, false);
   assert.equal(result[0].contents[0].name, 'John');
   assert.end();
@@ -26,13 +26,13 @@ tape('ParseDir.handleAsync', (assert) => {
   const pd = new ParseDir({});
   pd.handleAsync('test/fixture/json/users.json', (err, result) => {
     assert.equal(err, null);
-    assert.ok(result[0].relativePath);
-    assert.ok(result[0].filepath);
-    assert.ok(result[0].filename);
-    assert.ok(result[0].basename);
-    assert.ok(result[0].extension);
-    assert.ok(result[0].parsed);
-    assert.ok(result[0].raw);
+    assert.ok(result[0].relativePath, 'should extract required fields');
+    assert.ok(result[0].filepath, 'should extract required fields');
+    assert.ok(result[0].filename, 'should extract required fields');
+    assert.ok(result[0].basename, 'should extract required fields');
+    assert.ok(result[0].extension, 'should extract required fields');
+    assert.ok(result[0].parsed, 'should extract required fields');
+    assert.ok(result[0].raw, 'should extract required fields');
     assert.equal(result[0].isDirectory, false);
     assert.equal(result[0].contents[0].name, 'John');
     assert.end();
