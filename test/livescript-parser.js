e5d9346711529cf3ee02edb5f3617b04840f02f9
orthagonal <orthagonal1@gmail.com>
2017-11-13 21:39:23 -0600

    async await

--- test/livescript-parser.js
+++ test/livescript-parser.js
@@ -5,59 +5,60 @@ const fs = require('fs');
 
 const fixture = 'test/fixture/livescript/';
 
-test.skip('livescript should be parsed', (t) => {
-  t.plan(1);//
-  parseDir(`${fixture}users.ls`, (err, data) => {
-    t.equal(data[0].parsed);
-  });
+test.skip('livescript should be parsed', async (t) => {
+  t.plan(1);
+  const data = await parseDir(`${fixture}users.ls`);
+  t.equal(data[0].parsed);
 });
 
-test('livescript should contain filepath', (t) => {
+test('livescript should contain filepath', async (t) => {
   t.plan(1);
-  parseDir(`${fixture}users.ls`, (err, data) => {
+  try {
+    const data = await parseDir(`${fixture}users.ls`);
+  } catch (e) {
+    console.log('-')
+    console.log('-')
+    console.log(e)
     t.equal(data[0].filepath.indexOf(`${fixture}users.ls`) !== -1, true);
-  });
+  }
 });
 
-test('livescript should contain filename', (t) => {
+test('livescript should contain filename', async (t) => {
   t.plan(1);
-  parseDir(`${fixture}users.ls`, (err, data) => {
-    t.equal(data[0].filename, 'users.ls');
-  });
+  const data = await parseDir(`${fixture}users.ls`);
+  t.equal(data[0].filename, 'users.ls');
 });
 
-test('livescript should contain basename', (t) => {
+test('livescript should contain basename', async (t) => {
   t.plan(1);
-  parseDir(`${fixture}users.ls`, (err, data) => {
-    t.equal(data[0].basename, 'users');
-  });
+  const data = await parseDir(`${fixture}users.ls`);
+  t.equal(data[0].basename, 'users');
 });
 
-test('livescript should contain extension', (t) => {
+test('livescript should contain extension', async (t) => {
   t.plan(1);
-  parseDir(`${fixture}users.ls`, (err, data) => {
-    t.equal(data[0].extension, '.ls');
-  });
+  const data = await parseDir(`${fixture}users.ls`);
+  t.equal(data[0].extension, '.ls');
 });
 
-test('livescript should return raw output', (t) => {
+test('livescript should return raw output', async (t) => {
   t.plan(1);
-  parseDir(`${fixture}users.ls`, (err, data) => {
-    t.equal(data[0].raw, fs.readFileSync(`${fixture}users.ls`).toString());
-  });
+  const data = await parseDir(`${fixture}users.ls`);
+  t.equal(data[0].raw, fs.readFileSync(`${fixture}users.ls`).toString());
 });
 
-test.skip('livescript should return parsed output', (t) => {
+test.skip('livescript should return parsed output', async (t) => {
   t.plan(1);
-  parseDir(`${fixture}users.ls`, (err, data) => {
-    t.equal(data[0].contents[0].name, 'John');
-  });
+  const data = await parseDir(`${fixture}users.ls`);
+  t.equal(data[0].contents[0].name, 'John');
 });
 
 // Need to figure out a way to test this. Broken JS will just break everything and require just loads an empty object.
-test.skip('livescript should handle malformed livescript', (t) => {
+test.skip('livescript should handle malformed livescript', async (t) => {
   t.plan(1);
-  parseDir(fixture + 'malformed.ls', (err, data) => {
-    t.equal(!data[0].parsed);
-  });
+  try {
+    parseDir(fixture + 'malformed.ls');
+  } catch (e) {
+    t.notEqual(e, null);
+  }
 });
